7407c9d965499e786ae08c331e15c70a5d8c24e7
Abhi-Codes <androidabhishekanand@gmail.com>
2022-11-20 12:55:26 +0530

    Add changes

---
+++ src/main/java/com/abhicodes/customdialect/CustomDialectApplication.java
@@ -0,0 +1,13 @@
+package com.abhicodes.customdialect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomDialectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomDialectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhicodes/customdialect/CustomDialectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhicodes.customdialect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomDialectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
